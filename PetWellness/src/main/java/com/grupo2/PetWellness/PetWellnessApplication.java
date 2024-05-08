c249a0cc627c0ea432d7195f3d0e2043d10d400f
Adrian Cisneros <acisnerosb1@upao.edu.pe>
2024-05-07 23:54:57 -0500

    feat: Estructura inicial de PetWellness REST API

---
+++ PetWellness/src/main/java/com/grupo2/PetWellness/PetWellnessApplication.java
@@ -0,0 +1,13 @@
+package com.grupo2.PetWellness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetWellnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetWellnessApplication.class, args);
+	}
+
+}
---
+++ PetWellness/src/test/java/com/grupo2/PetWellness/PetWellnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo2.PetWellness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetWellnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
