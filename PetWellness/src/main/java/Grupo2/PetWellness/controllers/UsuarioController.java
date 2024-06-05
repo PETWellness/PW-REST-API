dac4110f8453bbec0c7c78189dcb71faa7664621
FluidityCore <100382574+FluidityCore@users.noreply.github.com>
2024-06-05 13:42:27 -0500

    Creacion de los controllers

---
+++ PetWellness/src/main/java/Grupo2/PetWellness/controllers/UsuarioController.java
@@ -0,0 +1,57 @@
+package Grupo2.PetWellness.controllers;
+
+import Grupo2.PetWellness.entitys.models.Usuario;
+import Grupo2.PetWellness.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public List<Usuario> getAllUsuarios() {
+        return usuarioService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getUsuarioById(@PathVariable Long id) {
+        Optional<Usuario> usuario = usuarioService.findById(id);
+        return usuario.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Usuario createUsuario(@RequestBody Usuario usuario) {
+        return usuarioService.save(usuario);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> updateUsuario(@PathVariable Long id, @RequestBody Usuario usuarioDetails) {
+        Optional<Usuario> usuario = usuarioService.findById(id);
+        if (usuario.isPresent()) {
+            Usuario updatedUsuario = usuario.get();
+            updatedUsuario.setOwnerFirstName(usuarioDetails.getOwnerFirstName());
+            updatedUsuario.setOwnerLastName(usuarioDetails.getOwnerLastName());
+            updatedUsuario.setEmail(usuarioDetails.getEmail());
+            updatedUsuario.setPassword(usuarioDetails.getPassword());
+            updatedUsuario.setImgprofile(usuarioDetails.getImgprofile());
+            usuarioService.save(updatedUsuario);
+            return ResponseEntity.ok(updatedUsuario);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUsuario(@PathVariable Long id) {
+        usuarioService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
